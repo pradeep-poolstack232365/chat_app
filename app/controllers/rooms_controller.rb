0e221941a15dbe41dccb4526235b81b034390686
pradeep-poolstack232365 <pradeep.kushwaha@poolstack.in>
2024-02-19 15:01:18 +0530

    private_chat

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,9 @@
 class RoomsController < ApplicationController
+
    def index
     # debugger
     @room = Room.new
-    @current_user = User.find(session[:user_id])
+    @current_user = User.find_by(id:  session[:user_id])
     redirect_to '/signin' unless @current_user
     @rooms = Room.public_rooms
     @users = User.all_except(@current_user)
@@ -28,10 +29,10 @@ class RoomsController < ApplicationController
    def create
     # debugger
     @room = Room.create(name: params["room"]["name"])
-    if @room.save
-    redirect_to @room, notice: 'Room was successfully created.'
-    else
-    head :no_content
-    end
+     if @room.save
+        redirect_to @room, notice: 'Room was successfully created.'
+     else
+        head :no_content
+     end
     end
 end
